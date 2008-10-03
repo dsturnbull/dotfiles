d86c35f8bce07b401311d6ccf365035d225b1a70
David Turnbull <dave@wessel.(none)>
2008-10-04 00:35:40 +1000

    simplify up xmonad conf a bit

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,10 +1,7 @@
 import XMonad
 
 main = xmonad $ defaultConfig {
-    borderWidth        = 2,
-    terminal           = "/usr/bin/gnome-terminal --hide-menubar",
-    normalBorderColor  = "#cccccc",
-    defaultGaps = [(18,0,0,0)],
-    focusedBorderColor = "#cd8b00",
-    modMask = mod4Mask
+  terminal = "/usr/bin/gnome-terminal --hide-menubar",
+  normalBorderColor = "#cccccc",
+  modMask = mod4Mask
 }
