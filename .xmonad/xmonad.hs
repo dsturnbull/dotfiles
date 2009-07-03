308346e422f701de98a6a7f0a476c78192946391
Dave Turnbull <dave@redcordial.org>
2009-07-03 23:54:12 +1000

    xmonad in osx

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,28 +1,16 @@
-import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import XMonad.Layout.ThreeColumns
 import System.IO
+import XMonad
+main = xmonad defaults
 
-myLayouts = ThreeCol 1 (3/100) (1/2)
-
-main = do
-  xmproc <- spawnPipe "/home/dave/.cabal/bin/xmobar"
-  xmonad $ defaultConfig {
-    manageHook = manageDocks <+> manageHook defaultConfig,
-    -- layoutHook = avoidStruts  $  layoutHook defaultConfig,
-    layoutHook = avoidStruts  $  myLayouts,
-    terminal = "/usr/bin/rxvt-unicode",
-    normalBorderColor = "#cccccc",
-    focusedBorderColor = "#cd8b00",
-    logHook = dynamicLogWithPP $ xmobarPP {
-      ppOutput = hPutStrLn xmproc,
-      ppTitle = xmobarColor "green" "" . shorten 50
-    }
-  } `additionalKeys`
-    [((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock"),
-      ((controlMask, xK_Print), spawn "scrot -s"),
-      ((0, xK_Print), spawn "scrot")
-    ]
+defaults = defaultConfig {
+    terminal = "/opt/local/bin/urxvt"
+  , borderWidth = 1
+  , normalBorderColor = "#cccccc"
+  , focusedBorderColor = "#cd8b00"
+  , modMask = mod4Mask
+}
