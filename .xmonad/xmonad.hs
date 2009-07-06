7999d6ebde34685b22f23f5f4116cfb89ca906d8
Dave Turnbull <dave@redcordial.org>
2009-07-07 09:41:40 +1000

    refactor xmonad.hs

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -20,30 +20,34 @@ import qualified XMonad.Prompt         as P
 main = do
     putEnv "BROWSER=firefox-x11-standalone"
     xmproc <- spawnPipe "xmobar $HOME/.xmonad/xmobar"
-    xmonad $ defaultConfig
-        { terminal           = "/opt/local/bin/urxvt"
-        , borderWidth        = 1
-        , normalBorderColor  = "#cccccc"
-        , focusedBorderColor = "#cd8b00"
-        , modMask            = mod4Mask
-        , focusFollowsMouse  = sloppyFocus
-        , manageHook         = manageDocks <+> manageHook defaultConfig
-        , layoutHook         = smartBorders . layoutHints . avoidStruts $ myLayouts
-        , logHook            = dynamicLogWithPP $ xmobarPP
-            { ppOutput = hPutStrLn xmproc
-            , ppTitle  = xmobarColor "green" "" . shorten 50
-            }
-        } `additionalKeys` myKeys
+    xmonad $ myConfig xmproc `additionalKeys` myKeys
 
+myConfig xmproc = defaultConfig
+    { terminal           = "/opt/local/bin/urxvt"
+    , borderWidth        = 1
+    , normalBorderColor  = "#cccccc"
+    , focusedBorderColor = "#cd8b00"
+    , modMask            = mod4Mask
+    , focusFollowsMouse  = sloppyFocus
+    , manageHook         = manageDocks <+> manageHook defaultConfig
+    , layoutHook         = smartBorders . layoutHints . avoidStruts $ myLayouts
+    , logHook            = dynamicLogWithPP $ xmobarPP
+        { ppOutput = hPutStrLn xmproc
+        , ppTitle  = xmobarColor "green" "" . shorten 50
+        }
+    }
 myKeys =
-    [ ((mod4Mask, xK_f), viewEmptyWorkspace)
-    , ((modShift, xK_f), tagToEmptyWorkspace)
-    , ((mod4Mask, xK_a), sendMessage MirrorExpand)
-    , ((mod4Mask, xK_z), sendMessage MirrorShrink)
-    , ((mod4Mask, xK_s), search)
-    , ((modShift, xK_s), sshPrompt defaultXPConfig)
+    [ ((modMask, xK_f), viewEmptyWorkspace)
+    , ((modShft, xK_f), tagToEmptyWorkspace)
+    , ((modMask, xK_a), sendMessage MirrorExpand)
+    , ((modMask, xK_z), sendMessage MirrorShrink)
+    , ((modMask, xK_s), search)
+    , ((modShft, xK_s), sshPrompt defaultXPConfig)
     ]
-    where modShift    = mod4Mask .|. shiftMask
+    where modMask     = mod4Mask
+          modShft     = modMask .|. shiftMask
+          modCtrl     = modMask .|. controlMask
+          modShCr     = modMask .|. shiftMask .|. controlMask
           search      = SM.submap $ searchMap $ S.promptSearch P.defaultXPConfig
           searchMap m = M.fromList $
               [ ((0, xK_g), m S.google)
@@ -56,11 +60,10 @@ myKeys =
               ]
 
 myLayouts = tiled' ||| Mirror tiled' ||| Full
-  where
-    nmaster  = 1     -- The default number of windows in the master pane
-    ratio    = 1/2   -- Default proportion of screen occupied by master pane
-    delta    = 3/100 -- Percent of screen to increment by when resizing panes
-    tiled'   = maximize $ ResizableTall nmaster delta ratio []
+    where nmaster = 1     -- The default number of windows in the master pane
+          ratio   = 1/2   -- Default proportion of screen occupied by master pane
+          delta   = 3/100 -- Percent of screen to increment by when resizing panes
+          tiled'  = maximize $ ResizableTall nmaster delta ratio []
 
 -- dodgy trackpad!
 sloppyFocus = False
