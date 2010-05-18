85ce35d23e48e5500dd3599d63445f02f8993ead
Dave Turnbull <dave@redcordial.org>
2010-05-18 17:11:16 +1000

    flymake-js tweak

--- .emacs.d/initialisers/flymake.el
+++ .emacs.d/initialisers/flymake.el
@@ -6,11 +6,10 @@
 (my-flymake-minor-mode)
 
 (load-file (concat vendor-dir "nxhtml/related/flymake-js.el"))
-;;(setq flymake-js-rhino-jar (expand-file-name "~/src/rhino/js.jar"))
-;;(setq flymake-js-rhino-use-jslint t)
 (setq flymake-js-engine 'jslintpy)
 (flymake-js-load)
 
 (add-hook 'espresso-mode-hook 'flymake-js-init)
 (add-hook 'find-file-hook 'flymake-find-file-hook)
 (add-hook 'ruby-mode-hook 'flymake-ruby-load)
+
