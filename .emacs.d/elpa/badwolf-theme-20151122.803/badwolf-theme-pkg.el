b620624fa874cfe14d62671f4ace083ae63f6364
David Turnbull <dturnbull@arbor.net>
2015-12-14 12:39:56 +1100

    commit packages(?)

---
+++ .emacs.d/elpa/badwolf-theme-20151122.803/badwolf-theme-pkg.el
@@ -0,0 +1,1 @@
+(define-package "badwolf-theme" "20151122.803" "Bad Wolf color theme" '((emacs "24")) :url "https://github.com/bkruczyk/badwolf-emacs" :keywords '("themes"))
---
+++ .emacs.d/elpa/flycheck-irony-20150728.1231/flycheck-irony-pkg.el
@@ -0,0 +1,1 @@
+(define-package "flycheck-irony" "20150728.1231" "Flycheck: C/C++ support via Irony" '((emacs "24.1") (flycheck "0.22") (irony "0.2.0alpha4")) :url "https://github.com/Sarcasm/flycheck-irony/" :keywords '("convenience" "tools" "c"))
